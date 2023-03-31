4154c0397f9d482b61724203b3239e063df0993e
rossbrownzzz <ross-brown@uiowa.edu>
2023-03-30 23:08:02 -0500

    neural networkbackpropogates. activation issues

--- network.py
+++ network.py
@@ -2,7 +2,8 @@ import random
 random.seed(123)
 
 class Network:
-    def __init__(self, numLayers=4, layerDim=[15,14,13,12], inputDim=11, outputDim=9, learningRate=0.08, lrDecay=0.0001, momentum=0.6, momentumDecay = 0.01, initialWeightsMax=0.15, trainBatchSize=1):
+    def __init__(self, numLayers=4, layerDim=[15,13,14,12], inputDim=11, outputDim=9, learningRate=0.1, lrDecay=0.00001, \
+                 momentum=0.6, momentumDecay = 0.01, initialWeightsMax=0.15, trainBatchSize=1):
         self.numLayers = numLayers
         self.layerDim = layerDim
         self.inputDim = inputDim
@@ -36,52 +37,64 @@ class Network:
 
 
     def predict(self, input):
-        return self.layers[0].activations(input)
+        act = input
+        for layer in self.layers:
+            act = layer.activations(act)
+        return self.outputLayer.activations(act)
 
 
-    def updateWeights(self, trainingGames):
-        for games in trainingGames:
-            gameResult = games[-1]
-            gameinfo = games[:-1]
-            for inputOutput in gameinfo:
-                boardState = inputOutput[0]
-                moveMade = inputOutput[1]
 
-                losses = self.loss(boardState, gameResult)
-                self.backPropogate(losses, moveMade)
+    def train(self, trainData):
+        for sample, result in trainData:
+            activations = []
+            act = sample
+            for layer in self.layers:
+                act = layer.activations(act)
+                activations.append(act)
+            activations.append(self.outputLayer.activations(act))
 
+            error = self.loss(activations[-1], result)
+            self.backPropogate(error, activations)
 
     ###### TODO try setting another neural network as the board state judge. make it regression rather than classification, and use the prospective increase or decrease in
     ###### board state value to get loss from every output, rather than just the move made.
     ###### train the regression network on board states encountered and whether it wound up being a win or a lose for that side.
-    def loss(self, pred, result):
-        # if won, it should have been higher
-        if result == 1:
-            return (pred + pred * self.learningRate) - pred
-        # if lost it should have been lower
-        elif result == -1:
-            return -(pred - (pred - pred * self.learningRate))
-        # if tie it should have been slightly lower
-        elif result == 0:
-            return -(pred - (pred - pred * self.learningRate/4))
-        
+    def loss(self, pred, true):
+        if len(pred)==1:
+            errors = [pred[0] - true]
+        else:
+            errors = []
+            for i in range(len(pred)):
+                errors.append(pred[i] - true[i])
+        return errors
 
-    def backPropogate(self, loss, outNode):
-        hiddenErrors = [0]*self.layerNodes
-        for i in range(self.layerNodes):
-            accum = 0
-            accum += self.outputWeights[outNode][i] * loss
-        
-        for i in range(self.layerNodes):
-            hiddenErrors[i] = accum * self.hiddenValue[i] * (1- self.hiddenValue[i])
-            for j in range(self.previousLayerNodes):
-                self.hiddenWeights[i][j] += hiddenErrors[i] * self.learningRate * self.momentum
-            self.outputWeights[outNode][i] += loss * self.learningRate * self.momentum
-        # decay momentum
-        if self.momentum > self.momentumDecay:
-            self.momentum = self.momentum - self.momentumDecay
-        if self.learningRate > self.lrDecay:
-            self.learningRate = self.learningRate - self.lrDecay
+
+    def backPropogate(self, error, activations):
+        activations.reverse()
+        deltas = [[]]
+        for outNode in range(len(activations[0])):
+            deltas[0].append(error[outNode] * activations[0][outNode] * (1-activations[0][outNode]))
+        i = 1
+        #print('DELTAS 0', deltas[0])
+        for layer in self.layers[::-1]:
+            #print(layer.hiddenWeights)
+            delta = []
+            for j in range(len(activations[i])):
+                #print('-------act', activations[i])
+                D = 0
+                for prevDelta in deltas[i-1]:
+                    D += activations[i][j] * prevDelta
+                D = D * activations[i][j] * (1 - activations[i][j]) 
+                delta.append(D)
+            deltas.append(delta)
+            #print(deltas)
+            for j in range((len(activations[i]))):
+                for node in range(len(layer.hiddenWeights[j])):
+                    #print(layer.hiddenWeights[j][node])
+                    layer.hiddenWeights[j][node] += -activations[i][j] * deltas[i][j] * self.learningRate
+                    #print('activ, delta, lr', -activations[i][j], deltas[i][j], self.learningRate)
+            #print("new", layer.hiddenWeights)
+            i += 1 
 
 
 
@@ -97,13 +110,10 @@ class Layer:
         self.previousLayer = previous
         if self.previousLayer is not None:
             self.previousLayerNodes = previous.layerNodes
-            self.hiddenWeights = [[random.randint(1,100)/100 * self.initialWeightsMax for i in range(self.previousLayerNodes)] for j in range(self.layerNodes)]
-            #self.hiddenBias = [random.randint(1,100)/100 * initialWeightsMax for i in range(layerNodes)]
+            self.hiddenWeights = [[random.randint(1,100)/100 * (-1 if random.random()>0.5 else 1) * self.initialWeightsMax for i in range(self.previousLayerNodes)] for j in range(self.layerNodes)]
         
         if self.nextLayer is not None:
             self.nextLayerNodes = next.layerNodes
-            self.outputWeights = [[random.randint(1,100)/100 * self.initialWeightsMax for i in range(self.layerNodes)] for j in range(self.nextLayerNodes)]
-            #self.outputBias = [random.randint(1,100)/100 * initialWeightsMax for i in range(outputNodes)]
        
 
     def activations(self, inputs):
@@ -111,13 +121,23 @@ class Layer:
         for i in range(self.layerNodes):
             accum = 0
             for j in range(self.previousLayerNodes):
-                accum += inputs[j] * self.hiddenWeights[i][j] #+ self.hiddenBias[i]
-            outputValue[i] = 1/(1+pow(2.718, -accum))
-        if self.nextLayer is not None:
-            return self.nextLayer.activations(outputValue)
-        else:    
-            return outputValue
+                accum += inputs[j] * self.hiddenWeights[i][j] 
+            outputValue[i] = 1/(1+pow(2.718, -accum)) 
+        #print('o', outputValue)
+        return outputValue
         
 
-test = Network()
-print(test.predict([0,1,1,1,1,2,1,0,1,1,0,1]))
+
+test = Network(inputDim=2, numLayers=2, layerDim=[2,2], outputDim=2)
+prev = test.predict([1,0])#[0,1,1,1,1,0,1,0,1,1,0,1])
+print(prev)
+
+for i in range(100):
+    test.train([[[1,0],[10,0]]])
+    #test.train([[[0,1,1,1,1,0,1,0,1,1,0,1], [1, 0]]])
+    curr = test.predict([1,0])
+    #curr = test.predict([0,1,1,1,1,0,1,0,1,1,0,1])
+    #print(curr)
+    print(curr[0]-prev[0], curr[1]-prev[1])
+    prev=curr
+print(curr)
